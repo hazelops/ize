5625aa32ba33be16d3d90a7e133611d2761ac883
Nikita <psihachina@gmail.com>
2021-12-16 13:38:25 +0300

    TOOLS-119 add run terraform init and plan for infra deploy commnad

--- internal/commands/deploy.go
+++ internal/commands/deploy.go
@@ -3,8 +3,10 @@ package commands
 import (
 	"fmt"
 
+	"github.com/hazelops/ize/internal/docker/terraform"
 	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 type deployCmd struct {
@@ -35,20 +37,47 @@ func (b *commandsBuilder) newDeployCmd() *deployCmd {
 			for pname, provider := range cc.config.Infra {
 				switch pname {
 				case "terraform":
-					var terraform map[string]hclTerraform = make(map[string]hclTerraform)
-					var t hclTerraform
-					mapstructure.Decode(provider, &t)
-					terraform["main"] = t
+					var tic terraformInfraConfig
+					mapstructure.Decode(provider, &tic)
 
-					for sname, subitem := range provider {
-						i, ok := subitem.(map[string]interface{})
-						if ok {
-							mapstructure.Decode(i, &t)
-							terraform[sname] = t
-						}
+					opts := terraform.Options{
+						ContainerName: "terraform",
+						Cmd:           []string{"init"},
+						Env: []string{
+							fmt.Sprintf("ENV=%v", cc.config.Env),
+							fmt.Sprintf("AWS_PROFILE=%v", tic.Profile),
+							fmt.Sprintf("TF_LOG=%v", viper.Get("TF_LOG")),
+							fmt.Sprintf("TF_LOG_PATH=%v", viper.Get("TF_LOG_PATH")),
+						},
+						TerraformVersion: tic.Version,
 					}
 
-					cc.log.Debugf("%s block: %s", pname, terraform)
+					err = terraform.Run(&cc.log, opts)
+					if err != nil {
+						cc.log.Errorf("terraform %s not completed", "init")
+						return err
+					}
+
+					opts = terraform.Options{
+						ContainerName: "terraform",
+						Cmd:           []string{"plan"},
+						Env: []string{
+							fmt.Sprintf("ENV=%v", cc.config.Env),
+							fmt.Sprintf("AWS_PROFILE=%v", tic.Profile),
+							fmt.Sprintf("TF_LOG=%v", viper.Get("TF_LOG")),
+							fmt.Sprintf("TF_LOG_PATH=%v", viper.Get("TF_LOG_PATH")),
+						},
+						TerraformVersion: tic.Version,
+					}
+
+					cc.log.Debugf("terraform run opts: %s", opts)
+
+					err = terraform.Run(&cc.log, opts)
+					if err != nil {
+						cc.log.Errorf("terraform %s not completed", "plan")
+						return err
+					}
+
 				default:
 					return fmt.Errorf("provider %s is not supported", pname)
 				}
@@ -63,7 +92,7 @@ func (b *commandsBuilder) newDeployCmd() *deployCmd {
 	return cc
 }
 
-type hclTerraform struct {
+type terraformInfraConfig struct {
 	RootDir string `mapstructure:"root_dir,optional"`
 	Version string `mapstructure:"terraform_version,optional"`
 	Region  string `mapstructure:"aws_region,optional"`
