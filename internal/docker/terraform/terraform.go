cbb508094ca9872f22f9b33e4051dde02ef12e18
Nikita <psihachina@gmail.com>
2022-01-24 12:46:48 +0300

    TOOLS-210 add display terrafrom output without log

--- internal/docker/terraform/terraform.go
+++ internal/docker/terraform/terraform.go
@@ -194,11 +194,10 @@ func Run(opts Options) error {
 			strErr = strings.ToLower(string(strErr[0])) + strErr[1:]
 			err = fmt.Errorf(strErr)
 		}
-		if logrus.GetLevel() >= 4 || strings.Contains(strings.Join(opts.Cmd, ""), "output") {
-			fmt.Println(scanner.Text())
-		}
 		if f != nil {
 			f.WriteString(scanner.Text())
+		} else {
+			fmt.Println(scanner.Text())
 		}
 	}
 
