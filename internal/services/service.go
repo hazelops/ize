4197612d0a7587107005b98658fefeb73a2e065c
Nikita <psihachina@gmail.com>
2022-02-27 12:18:33 +0300

    IZE-114 refactor service deploy

---
+++ internal/services/service.go
@@ -0,0 +1,32 @@
+package services
+
+import (
+	"fmt"
+
+	"github.com/hazelops/ize/pkg/terminal"
+)
+
+type Service struct {
+	Name      string
+	Type      string
+	Path      string
+	DependsOn []string               `mapstructure:"depends_on"`
+	Body      map[string]interface{} `mapstructure:",remain"`
+}
+
+func (svs *Service) Deploy(sg terminal.StepGroup, ui terminal.UI) error {
+	var deployment Deployment
+
+	switch svs.Type {
+	case "ecs":
+		deployment = NewECSDeployment(*svs)
+	default:
+		return fmt.Errorf("services type of %s not supported", svs.Type)
+	}
+
+	err := deployment.Deploy(sg, ui)
+	if err != nil {
+		return err
+	}
+
+	return nil
