f7b6a5d9a0c07dc3c752d0f43fcb99ebac6a8006
psihachina <psihachina@gmail.com>
2022-06-30 10:19:04 +0300

    describe new structures for new file configuration design

--- internal/config/app.go
+++ internal/config/app.go
@@ -1,6 +1,37 @@
 package config
 
 type App struct {
-	Type string `mapstructure:",omitempty"`
-	Path string `mapstructure:",omitempty"`
+	Type                    string   `mapstructure:",omitempty"`
+	Name                    string   `mapstructure:",omitempty"`
+	Unsafe                  bool     `mapstructure:",omitempty"`
+	Path                    string   `mapstructure:",omitempty"`
+	Image                   string   `mapstructure:",omitempty"`
+	Cluster                 string   `mapstructure:",omitempty"`
+	TaskDefinitionRevision  string   `mapstructure:"task_definition_revision"`
+	Timeout                 int      `mapstructure:",omitempty"`
+	File                    string   `mapstructure:",omitempty"`
+	NodeVersion             string   `mapstructure:"node_version"`
+	Env                     []string `mapstructure:",omitempty"`
+	SLSNodeModuleCacheMount string   `mapstructure:",omitempty"`
+	CreateDomain            bool     `mapstructure:"create_domain"`
+}
+
+type Ecs struct {
+	Name                   string `mapstructure:",omitempty"`
+	Unsafe                 bool   `mapstructure:",omitempty"`
+	Path                   string `mapstructure:",omitempty"`
+	Image                  string `mapstructure:",omitempty"`
+	Cluster                string `mapstructure:",omitempty"`
+	TaskDefinitionRevision string `mapstructure:"task_definition_revision"`
+	Timeout                int    `mapstructure:",omitempty"`
+}
+
+type Serverless struct {
+	Name                    string   `mapstructure:",omitempty"`
+	File                    string   `mapstructure:",omitempty"`
+	NodeVersion             string   `mapstructure:"node_version"`
+	Env                     []string `mapstructure:",omitempty"`
+	Path                    string   `mapstructure:",omitempty"`
+	SLSNodeModuleCacheMount string   `mapstructure:",omitempty"`
+	CreateDomain            bool     `mapstructure:"create_domain"`
 }
--- internal/config/project.go
+++ internal/config/project.go
@@ -9,6 +9,7 @@ type Project struct {
 	LogLevel         string `mapstructure:"log_level,omitempty"`
 	PlainText        bool   `mapstructure:"plain_text,omitempty"`
 	PreferRuntime    string `mapstructure:"prefer_runtime,omitempty"`
+	Tag              string `mapstructure:",omitempty"`
 
 	home     string `mapstructure:",omitempty"`
 	rootDir  string `mapstructure:"root_dir,omitempty"`
@@ -18,4 +19,8 @@ type Project struct {
 
 	Infra *Infra          `mapstructure:",omitempty"`
 	App   *map[string]App `mapstructure:",omitempty"`
+
+	Terraform  *map[string]Terraform  `mapstructure:",omitempty"`
+	Ecs        *map[string]Ecs        `mapstructure:",omitempty"`
+	Serverless *map[string]Serverless `mapstructure:",omitempty"`
 }
