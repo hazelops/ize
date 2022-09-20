a5b474b0d81276b198adf318aa5c7cf3f77fbf84
Elizaveta <elizraota@gmail.com>
2022-09-20 14:36:20 +0300

    switching between tab fields handled

--- website/components/tab/tab.js
+++ website/components/tab/tab.js
@@ -1,32 +1,52 @@
 import { useState } from 'react'
+import { installationMenu } from '../../utilities/installationMenu'
 
-function TabButton(props) {
-    const [selected, setSelected] = useState()
-    const border = selected ? "border-b-2 border-blue-500" : ""
+function TabButton({ field, id, onClick, active }) {
+    const border = active == id ? "border-b-2 border-blue-500" : ""
 
     return (
         <button 
-            className={`h-10 px-4 py-2 -mb-px text-sm text-center text-blue-600 bg-transparent sm:text-base whitespace-nowrap focus:outline-none ${border}`}
-            onClick={props.onClick}
+            className={`h-10 px-4 py-2 -mb-px text-sm text-center text-blue-600 bg-transparent sm:text-base whitespace-nowrap focus:outline-none hover:bg-gray-200 ${border}`}
+            onClick={() => onClick(id)}
         >
-            {props.platform}
+            {field}
         </button>
     )
 }
 
+function TabContent({ children }) {
+    return (
+        <div>{children}</div>
+    )
+}
+
+//---------------------------------------------------------
+
 export default function Tab() {
-    const handleClick = function() {
-        return
+    const [active, setActive] = useState(0)
+
+    const handleClick = function(id) {
+        return setActive(id)
     }
 
+    const listButtons = installationMenu.map(el => {
+        const index = installationMenu.indexOf(el)
+        return (
+            <TabButton key={index}
+                field={el} 
+                onClick={handleClick} 
+                id={index}
+                active={active} 
+            />
+        )
+    })
+
     return (
-            <div className="inline-flex w-2/3 justify-center">
+            <div className="flex flex-col w-2/3 items-center">
                 <div className="inline-flex w-1/2 justify-between border-b border-gray-200 h-fit">
-                    <TabButton platform="MacOS" />
-                    <TabButton platform="Ubuntu" />
-                    <TabButton platform="Source" />
-                    <TabButton platform="Autocomplete" />
+                    {listButtons}
                 </div>
+                <TabContent></TabContent>
             </div> 
     )
 }
