6e828fc283917d3d6cd4c88aedc108df16818738
psihachina <psihachina@gmail.com>
2022-10-04 14:25:45 +0300

    added new jsonschema validator
    Old validator unsupported new drafts and bad handle errors.

--- internal/schema/schema.go
+++ internal/schema/schema.go
@@ -2,9 +2,9 @@ package schema
 
 import (
 	"fmt"
-	"strings"
-
+	"github.com/santhosh-tekuri/jsonschema"
 	"github.com/xeipuuv/gojsonschema"
+	"strings"
 
 	// Enable support for embedded static resources
 	_ "embed"
@@ -13,26 +13,36 @@ import (
 //go:embed ize-spec.json
 var Schema string
 
-const (
-	jsonschemaOneOf = "number_one_of"
-	jsonschemaAnyOf = "number_any_of"
-)
-
 // Validate uses the jsonschema to validate the configuration
 func Validate(config map[string]interface{}) error {
-	schemaLoader := gojsonschema.NewStringLoader(Schema)
-	dataLoader := gojsonschema.NewGoLoader(config)
-
-	result, err := gojsonschema.Validate(schemaLoader, dataLoader)
-	if err != nil {
-		return err
+	compiler := jsonschema.NewCompiler()
+	compiler.Draft = jsonschema.Draft7
+	if err := compiler.AddResource("schema.json", strings.NewReader(Schema)); err != nil {
+		panic(err)
 	}
-
-	if !result.Valid() {
-		return toError(result)
+	schema, err := compiler.Compile("schema.json")
+	if err != nil {
+		panic(err)
+	}
+	err = schema.ValidateInterface(config)
+	if err != nil {
+		i, m := GetErrorMessage(err.(*jsonschema.ValidationError))
+		if i == "#" {
+			i = "root"
+		} else {
+			i = strings.ReplaceAll(i[2:], "/", ".")
+		}
+		return fmt.Errorf("%s in %s", m, i)
 	}
 
 	return nil
+}
+
+func GetErrorMessage(err *jsonschema.ValidationError) (string, string) {
+	if len(err.Causes) == 0 {
+		return err.InstancePtr, err.Message
+	}
+	return GetErrorMessage(err.Causes[0])
 }
 
 func GetJsonSchema() interface{} {
@@ -44,90 +54,4 @@ func GetJsonSchema() interface{} {
 	}
 
 	return json
-}
-
-func toError(result *gojsonschema.Result) error {
-	err := getMostSpecificError(result.Errors())
-	return err
-}
-
-func (err validationError) Error() string {
-	description := getDescription(err)
-	return fmt.Sprintf("%s %s", err.parent.Field(), description)
-}
-
-func getDescription(err validationError) string {
-	switch err.parent.Type() {
-	case "invalid_type":
-		if expectedType, ok := err.parent.Details()["expected"].(string); ok {
-			return fmt.Sprintf("must be a %s", humanReadableType(expectedType))
-		}
-	case jsonschemaOneOf, jsonschemaAnyOf:
-		if err.child == nil {
-			return err.parent.Description()
-		}
-		return err.child.Description()
-	}
-	return err.parent.Description()
-}
-
-func humanReadableType(definition string) string {
-	if definition[0:1] == "[" {
-		allTypes := strings.Split(definition[1:len(definition)-1], ",")
-		for i, t := range allTypes {
-			allTypes[i] = humanReadableType(t)
-		}
-		return fmt.Sprintf(
-			"%s or %s",
-			strings.Join(allTypes[0:len(allTypes)-1], ", "),
-			allTypes[len(allTypes)-1],
-		)
-	}
-	if definition == "object" {
-		return "mapping"
-	}
-	if definition == "array" {
-		return "list"
-	}
-	return definition
-}
-
-func getMostSpecificError(errors []gojsonschema.ResultError) validationError {
-	mostSpecificError := 0
-	for i, err := range errors {
-		if specificity(err) > specificity(errors[mostSpecificError]) {
-			mostSpecificError = i
-			continue
-		}
-
-		if specificity(err) == specificity(errors[mostSpecificError]) {
-			// Invalid type errors win in a tie-breaker for most specific field name
-			if err.Type() == "invalid_type" && errors[mostSpecificError].Type() != "invalid_type" {
-				mostSpecificError = i
-			}
-		}
-	}
-
-	if mostSpecificError+1 == len(errors) {
-		return validationError{parent: errors[mostSpecificError]}
-	}
-
-	switch errors[mostSpecificError].Type() {
-	case "number_one_of", "number_any_of":
-		return validationError{
-			parent: errors[mostSpecificError],
-			child:  errors[mostSpecificError+1],
-		}
-	default:
-		return validationError{parent: errors[mostSpecificError]}
-	}
-}
-
-func specificity(err gojsonschema.ResultError) int {
-	return len(strings.Split(err.Field(), "."))
-}
-
-type validationError struct {
-	parent gojsonschema.ResultError
-	child  gojsonschema.ResultError
 }
