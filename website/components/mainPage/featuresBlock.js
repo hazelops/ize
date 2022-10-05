6d10c0c062fad51a0f6b5b8945864b5c6a1273b9
Lixxies <elizraota@gmail.com>
2022-10-05 18:08:01 +0300

    outer padding/margin reduced

--- website/components/mainPage/featuresBlock.js
+++ website/components/mainPage/featuresBlock.js
@@ -72,8 +72,8 @@ export default function FeaturesBlock({ extraData, features }) {
 
     return (
         <section className="text-gray-600 body-font">
-            <div className="container px-5 py-24 mx-auto">
-                <div className="flex flex-wrap w-full mb-10 flex-col items-center text-center">
+            <div className="container px-5 mx-auto">
+                <div className="flex flex-wrap w-full flex-col items-center text-center">
                     <h1 id="features" className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">{header}</h1>
                 </div>
 
