55c38802deb4c96c8347580b52cb089baa50e485
psihachina <psihachina@gmail.com>
2022-10-13 16:33:38 +0300

    fixed unit test

--- internal/commands/ci_test.go
+++ internal/commands/ci_test.go
@@ -1,12 +1,23 @@
 package commands
 
 import (
+	_ "embed"
 	"github.com/hazelops/ize/internal/config"
+	"github.com/hazelops/ize/internal/generate"
+	"os"
+	"path/filepath"
 	"testing"
 	"text/template"
 )
 
-func TestTemplate_Execute(t1 *testing.T) {
+func TestTemplate_Execute(t *testing.T) {
+	tempDir, _ := os.MkdirTemp("", "test")
+	_, err := generate.GenerateFiles("boilerplate-template", filepath.Join(tempDir, "template"))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
 	type fields struct {
 		Path    string
 		FuncMap template.FuncMap
@@ -22,7 +33,7 @@ func TestTemplate_Execute(t1 *testing.T) {
 		wantErr bool
 	}{
 		{name: "success", fields: fields{
-			Path: "./testdata/testrepo",
+			Path: filepath.Join(tempDir, "template"),
 			FuncMap: map[string]any{
 				"env":       func() string { return "dev" },
 				"namespace": func() string { return "testnut" },
@@ -33,16 +44,16 @@ func TestTemplate_Execute(t1 *testing.T) {
 				AwsProfile: "default",
 				AwsRegion:  "us-east-2",
 			},
-		}, args: args{dir: "./testdata/target"}, wantErr: false},
+		}, args: args{dir: filepath.Join(tempDir, "target")}, wantErr: false},
 	}
 	for _, tt := range tests {
-		t1.Run(tt.name, func(t1 *testing.T) {
-			t := &Template{
+		t.Run(tt.name, func(t1 *testing.T) {
+			tmpl := &Template{
 				Path:    tt.fields.Path,
 				FuncMap: tt.fields.FuncMap,
 				Data:    tt.fields.Data,
 			}
-			if err := t.Execute(tt.args.dir); (err != nil) != tt.wantErr {
+			if err := tmpl.Execute(tt.args.dir); (err != nil) != tt.wantErr {
 				t1.Errorf("Execute() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
