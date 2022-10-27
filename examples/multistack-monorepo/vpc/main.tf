a77d79ae92d49fbcab98541e0ad5970ae44597e8
Dmitry Borisov <dmitry.borisov@hazelops.com>
2022-10-27 16:43:38 +0300

    Add multistack monorepo

---
+++ examples/multistack-monorepo/vpc/main.tf
@@ -0,0 +1,32 @@
+module "vpc" {
+  source  = "registry.terraform.io/terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = "${var.env}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs = [
+    "${var.aws_region}a",
+    "${var.aws_region}b",
+
+  ]
+  public_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  private_subnets = [
+    "10.0.3.0/24",
+    "10.0.4.0/24"
+  ]
+
+  enable_nat_gateway         = true
+  single_nat_gateway         = true
+  enable_dns_hostnames       = true
+  manage_default_network_acl = true
+  default_network_acl_name   = "${var.env}-${var.namespace}"
+  tags = {
+    Terraform = "true"
+    Env       = var.env
+  }
+}
