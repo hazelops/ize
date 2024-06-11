2aa7b4db3ddc8bdc922165a5196421bd84398a11
Dmitry Kireev <dmitry@hazelops.com>
2024-06-11 04:22:22 +0000

    Fix E2E: WIP

--- examples/bastion-tunnel-monorepo/.ize/env/testnut/main.tf
+++ examples/bastion-tunnel-monorepo/.ize/env/testnut/main.tf
@@ -16,7 +16,7 @@ module "vpc" {
   name = "${var.env}-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a"]
+  azs             = ["${var.aws_region}a"]
   public_subnets  = ["10.0.1.0/24"]
   private_subnets = ["10.0.2.0/24"]
 
